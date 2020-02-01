095e70cb335606014ef4c448307b7b3bbf239ce1
Tarasiy <dirigablenorge@gmail.com>
2020-02-01 20:41:32 +0400

    add clients part

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'materialize-css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Short link application</h1>
+    </div>
+  );
+}
+
+export default App;
